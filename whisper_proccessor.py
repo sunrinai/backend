12d30f0114b4a95f8b16d50d812cb028b81fd770
Yellowstrawberrys <dirjhhsg@gmail.com>
2025-01-05 23:22:07 +0900

    fix: medium -> turbo

--- whisper_proccessor.py
+++ whisper_proccessor.py
@@ -4,6 +4,6 @@ def transcribe_audio(file_path):
     """
     Whisper를 사용하여 텍스트 변환 (전체 텍스트 출력)
     """
-    model = whisper.load_model("medium")
+    model = whisper.load_model("turbo")
     result = model.transcribe(file_path)
     return result["text"]
